8c4b12e253db67b9575295eb4a0fbb3f3de6b924
tonyhzw <tony_join@163.com>
2018-03-19 21:29:11 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+
+
+var app = express();
+
+app.use(express.static(__dirname+'/public'));
+
+app.get('/', function (req, res) {
+   res.sendFile( __dirname + "/"+ "test1.html" );
+})
+
+var server = app.listen(8081, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("应用实例，访问地址为 http://%s:%s", host, port)
+
+})
